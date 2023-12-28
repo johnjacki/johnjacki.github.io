890c99e229c3fb7419e1f4c9ebf56f6cc53d71a4
Elijah Ignacio <engignacio@spotzer.com>
2023-12-29 07:42:22 +0800

    google sheet

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 (function($){
+    
     // COUNTDOWN
     $('#countdown').countdown('2024/02/08 15:30:00').on('update.countdown', function(event) {
         if(event.offset.totalDays > 0)
@@ -17,5 +18,32 @@
         $('#countdown .data .day h2').html('00');
         $('#countdown .data .hour h2').html('00');
         $('#countdown .data .min h2').html('00');
+    });
+
+    // RSVP
+    $(document).on('submit','#rsvp form',function(e){
+        e.preventDefault();
+        try{
+            $.get('https://script.google.com/macros/s/AKfycbzAM71-JdtQ-GfMHccgOoPQp61tXF0L7tPQIlEIk2SPYlWpbHV7o_Vnqcmc1ngrQsL-/exec',
+                {data:JSON.stringify({
+                    'First Name':$('#rsvp form input[name="fname"]').val(),
+                    'Last Name':$('#rsvp form input[name="lname"]').val(),
+                    'Email':$('#rsvp form input[name="email"]').val(),
+                    'Contact Number':$('#rsvp form input[name="phone"]').val(),
+                    'Will you be able to attend our wedding?':$('#rsvp form input[name="going"]:checked').val(),
+                    'Message to the Bride & Groom':$('#rsvp form #message').val(),
+                })},
+                function(response, textStatus, jqXHR){
+                    if(response.success){
+                        console.log('Your RSVP is submitted');
+                    }else{
+                        console.log('Unable to submit RSVP');
+                    }
+                }
+            );
+        }catch(e){
+            console.log(e);
+        }
+        
     });
 })(jQuery);
