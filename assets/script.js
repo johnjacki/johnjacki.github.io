2962edd243a9de9f97315b7a9b7887a6e9986426
Elijah Ignacio <engignacio@spotzer.com>
2023-12-29 00:43:31 +0800

    fix countdown timer

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,21 @@
 (function($){
-    var countDownDate = new Date("Feb 8, 2024 15:30:00").getTime();
-    var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-            
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        
-        if(days > 0) $('#countdown .data .day h2').html(days);
-        else $('#countdown .data .day h2').html('00');
-        if(hours > 0) $('#countdown .data .hour h2').html(hours);
-        else $('#countdown .data .hour h2').html('00');
-        if(minutes > 0) $('#countdown .data .min h2').html(minutes);
-        else $('#countdown .data .hour h2').html('00');
-    }, 1000);
+    // COUNTDOWN
+    $('#countdown').countdown('2024/02/08 15:30:00').on('update.countdown', function(event) {
+        if(event.offset.totalDays > 0)
+            $('#countdown .data .day h2').html(event.offset.totalDays.toString().padStart(2, '0'));
+        else
+            $('#countdown .data .day h2').html('00');
+        if(event.offset.totalHours > 0)
+            $('#countdown .data .hour h2').html(event.offset.hours.toString().padStart(2, '0'));
+        else
+            $('#countdown .data .hour h2').html('00');
+        if(event.offset.totalMinutes > 0)
+            $('#countdown .data .min h2').html(event.offset.minutes.toString().padStart(2, '0'));
+        else
+            $('#countdown .data .min h2').html('00');    
+    }).on('finish.countdown', function(event) {
+        $('#countdown .data .day h2').html('00');
+        $('#countdown .data .hour h2').html('00');
+        $('#countdown .data .min h2').html('00');
+    });
 })(jQuery);
