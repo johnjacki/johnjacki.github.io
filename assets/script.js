c51c80d22b9d3fc70a4da9541afe3dd4f4837715
Elijah Ignacio <engignacio@spotzer.com>
2023-12-29 09:02:15 +0800

    update rsvp

--- assets/script.js
+++ assets/script.js
@@ -23,27 +23,34 @@
     // RSVP
     $(document).on('submit','#rsvp form',function(e){
         e.preventDefault();
+        $('#rsvp form button[type="submit"]').prop('disabled',true);
+        $('#rsvp form button[type="submit"]').html('<img src="images/loader.gif" width="16" height="16"/>');
         try{
-            $.get('https://script.google.com/macros/s/AKfycbzAM71-JdtQ-GfMHccgOoPQp61tXF0L7tPQIlEIk2SPYlWpbHV7o_Vnqcmc1ngrQsL-/exec',
-                {data:JSON.stringify({
+            $.get('https://script.google.com/macros/s/AKfycbwwfiCs9Te5zZNx6hNi8FIujCoDyosgRQi8M1PzcSZlSJg1bEA61hf8iwMEkgsfU2bH/exec',
+                {data:{
                     'First Name':$('#rsvp form input[name="fname"]').val(),
                     'Last Name':$('#rsvp form input[name="lname"]').val(),
                     'Email':$('#rsvp form input[name="email"]').val(),
                     'Contact Number':$('#rsvp form input[name="phone"]').val(),
                     'Will you be able to attend our wedding?':$('#rsvp form input[name="going"]:checked').val(),
                     'Message to the Bride & Groom':$('#rsvp form #message').val(),
-                })},
+                }},
                 function(response, textStatus, jqXHR){
+                    $('#rsvp form button[type="submit"]').prop('disabled',false);
+                    $('#rsvp form button[type="submit"]').html('Send');
+                    
                     if(response.success){
-                        console.log('Your RSVP is submitted');
+                        $('#rsvp form').prepend("<p class='alert alert-success'>Thank you for sending your response</p>")
                     }else{
-                        console.log('Unable to submit RSVP');
+                        $('#rsvp form').prepend("<p class='alert alert-danger'>We are unable to reserve your seat, please contact John or Jacki</p>")
                     }
-                }
+                    setInterval(function(){
+                        $('#rsvp form .alert').remove();
+                    },10000);
+                }, 'json'
             );
         }catch(e){
             console.log(e);
         }
-        
     });
 })(jQuery);
